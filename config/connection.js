1dcdae11431d7508a6be831a704515537cee9c50
Boris Cabrera <72274293+boro101094@users.noreply.github.com>
2023-07-02 15:27:46 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 const Sequelize = require('sequelize');
-
-//allow for use of .env data
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
-    host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql',
-    port: 3306
-  });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    'a3fwv2hacx83h9nk',
+   'u7xyfvei7977sgof',
+    'crwampgtnd9057o8',
+    {
+      host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
 
 module.exports = sequelize;
