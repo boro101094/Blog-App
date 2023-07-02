bae717fa564a6f2c9d6a043883b595e7a8a02962
Boris Cabrera <72274293+boro101094@users.noreply.github.com>
2023-07-02 15:21:15 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
-    host: 'localhost',
+    host: 'l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
   });
